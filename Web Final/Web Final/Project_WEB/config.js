bc6ca3107d8afc79ce5e331f20f1292303a24646
badgaldogii <93678153+badgaldogii@users.noreply.github.com>
2023-06-04 20:50:28 +0300

    Last version

---
+++ Web Final/Web Final/Project_WEB/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_jlXgGBGcK', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '75dbm0msai65bldt9gc2c4f6b5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://3a883n7x4a.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
